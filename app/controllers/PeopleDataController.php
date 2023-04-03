4d6dd5d38794ea120f93ded7617b21580f6afc89
rojasricor <rojasricor@gmail.com>
2023-04-02 22:44:22 -0500

    optimized small things

--- app/controllers/PeopleDataController.php
+++ app/controllers/PeopleDataController.php
@@ -4,12 +4,10 @@ namespace app\controllers;
 
 class PeopleDataController
 {
-
   static function save($id, $date, $start_date, $end_date, $time, $status, $color)
   {
     $db = Database::get();
     $statement = $db->prepare("INSERT INTO people_schedule VALUES (?, ?, ?, ?, ?, ?, ?)");
     return $statement->execute([$id, $date, $start_date, $end_date, $time, $status, $color]);
   }
-
 }
