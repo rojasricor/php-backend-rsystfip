65c47422e2facd8d2fa619818e21e93fc3ff2ef9
rojasricor <rojasricor@gmail.com>
2023-04-20 19:32:55 -0500

    database aws

--- app/models/DatabaseModel.php
+++ app/models/DatabaseModel.php
@@ -8,15 +8,15 @@ class DatabaseModel
 {
   public static function get()
   {
-    $pass = EnvModel::get('MYSQL_PSW');
-    $user = EnvModel::get('MYSQL_USER');
-    $dnam = EnvModel::get('MYSQL_DB');
-    $host = EnvModel::get('MYSQL_HOST');
-    $dbas = new PDO("mysql:host=$host;dbname=$dnam", $user, $pass);
-    $dbas->query('set names utf8;');
-    $dbas->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $dbas->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbas->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    return $dbas;
+    $dsn = "mysql:host=". EnvModel::get('HOST') . ";dbname=" . EnvModel::get('DATABASE');
+    $options = array(
+      PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt"
+    );
+    $pdo = new PDO($dsn, EnvModel::get('USERNAME'), EnvModel::get('PASSWORD'), $options);
+    $pdo->query('set names utf8;');
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    return $pdo;
   }
 }
