23545d54d03bfce3e5bf588fb85fb00f121003e2
rojasricor <rojasricor@gmail.com>
2023-04-21 21:12:18 -0500

    optimizing db

--- app/models/PeopleModel.php
+++ app/models/PeopleModel.php
@@ -6,14 +6,14 @@ class PeopleModel extends BaseModel
 {
   public function save($name, $tipo, $numero, $asunt)
   {
-    $statement = $this->db->prepare("INSERT INTO people(name, id_doc, num_doc, text_asunt) VALUES (?, ?, ?, ?)");
+    $statement = $this->db->prepare("INSERT INTO people(name, document_id, document_number, come_asunt) VALUES (?, ?, ?, ?)");
     return $statement->execute([$name, $tipo, $numero, $asunt]);
   }
 
-  public function schedule($name, $tipo, $numero, $type_person, $facultad, $asunt, $color, $date, $start, $end, $status)
+  public function schedule($name, $tipo, $numero, $type_person, $facultie_id, $asunt, $color, $date, $start, $end, $status)
   {
     $statement = $this->db->prepare("INSERT INTO people VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $statement->execute([NULL, $name, $tipo, $numero, $type_person, $facultad, $asunt]);
+    $statement->execute([NULL, $name, $tipo, $numero, $type_person, $facultie_id, $asunt]);
     $lastId = $this->getLast()->id;
     $timeModel = new TimeModel();
     $date ? $date : $date = $timeModel->todayDate();
@@ -48,21 +48,21 @@ class PeopleModel extends BaseModel
     return $statement->fetchObject();
   }
 
-  public function update($name, $tipo, $numero, $type_person, $facultad, $asunt, $id)
+  public function update($name, $tipo, $numero, $type_person, $facultie_id, $asunt, $id)
   {
-    $statement = $this->db->prepare("UPDATE people SET name = ?, id_doc = ?, num_doc = ?, person_type = ?, facultad = ?, text_asunt = ?  WHERE id = ?");
-    return $statement->execute([$name, $tipo, $numero, $type_person, $facultad, $asunt, $id]);
+    $statement = $this->db->prepare("UPDATE people SET name = ?, document_id = ?, document_number = ?, category_id = ?, facultie_id = ?, come_asunt = ?  WHERE id = ?");
+    return $statement->execute([$name, $tipo, $numero, $type_person, $facultie_id, $asunt, $id]);
   }
 
   public function getAll()
   {
-    $statement = $this->db->query("SELECT p.id, p.name, d.document as ty_doc, c.category, p.facultad, d.description, p.num_doc, f.name AS fac, p.text_asunt FROM people p, documents d, faculties f, categories c WHERE p.id_doc = d.id AND p.facultad = f.id AND p.person_type = c.id ORDER BY p.id DESC");
+    $statement = $this->db->query("SELECT p.id, p.name, d.document as ty_doc, c.category, p.facultie_id, d.description, p.document_number, f.facultie, p.come_asunt FROM people p, documents d, faculties f, categories c WHERE p.document_id = d.id AND p.facultie_id = f.id AND p.category_id = c.id ORDER BY p.id DESC");
     return $statement->fetchAll();
   }
 
   public function getCancelled()
   {
-    $statement = $this->db->query("SELECT p.id, p.name, d.document as ty_doc, c.category, p.facultad, d.description, p.num_doc, f.name AS fac, l.cancelled_asunt FROM people p, documents d, faculties f, categories c, cancelled l, scheduling s WHERE p.id_doc = d.id AND p.id = l.person_id AND s.person_id = l.person_id AND s.status = 'cancelled' AND p.facultad = f.id AND p.person_type = c.id ORDER BY p.id DESC");
+    $statement = $this->db->query("SELECT p.id, p.name, d.document as ty_doc, c.category, p.facultie_id, d.description, p.document_number, f.facultie, l.cancelled_asunt FROM people p, documents d, faculties f, categories c, cancelled l, scheduling s WHERE p.document_id = d.id AND p.id = l.person_id AND s.person_id = l.person_id AND s.status = 'cancelled' AND p.facultie_id = f.id AND p.category_id = c.id ORDER BY p.id DESC");
     return $statement->fetchAll();
   }
 
